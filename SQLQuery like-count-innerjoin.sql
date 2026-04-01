5bb8b53a460e38964e604d6d2e0b126776277fa4
CRISLEI JENUINO <crisleikeli@hotmail.com>
2026-04-01 20:39:14 -0300

    at
    atividade like, count, innerjoin

---
+++ SQLQuery like-count-innerjoin.sql
@@ -0,0 +1,9 @@
+SELECT * FROM professores_crisleikeli WHERE nome LIKE 'A%'
+
+SELECT * FROM alunos_crisleikeli WHERE nome LIKE '%A'
+
+SELECT * FROM disciplinas_crisleikeli WHERE	nome LIKE '%Mobile%'
+
+SELECT * FROM Professores_crisleikeli WHERE email LIKE '%gmail.com'
+
+SELECT * FROM alunos_crisleikeli COUNT 
