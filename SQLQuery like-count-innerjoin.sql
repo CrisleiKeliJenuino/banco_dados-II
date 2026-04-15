7575815a7e6bc568ecfc3283806ab2bdda074be6
CRISLEI KELI APARECIDA RIMENA JENUINO <crislei.jenuino>
2026-04-15 19:17:50 -0300

    Update SQLQuery like-count-innerjoin.sql

--- SQLQuery like-count-innerjoin.sql
+++ SQLQuery like-count-innerjoin.sql
@@ -6,4 +6,25 @@ SELECT * FROM disciplinas_crisleikeli WHERE	nome LIKE '%Mobile%'
 
 SELECT * FROM Professores_crisleikeli WHERE email LIKE '%gmail.com'
 
-SELECT * FROM alunos_crisleikeli COUNT 
+SELECT COUNT(*) AS Total FROM alunos_crisleikeli 
+
+SELECT a.nome AS 'Nome do Aluno'
+		,t.nome AS 'Nome da Turma' FROM alunos_crisleikeli a 
+INNER JOIN turmas_crisleikeli t ON a.turma_id = t.id
+
+SELECT a.nome  AS 'Nome do Aluno'
+	  ,t.nome  AS 'Nome da Turma'
+	  ,t.turno AS 'Nome do Turno'		 
+FROM alunos_crisleikeli a
+INNER JOIN turmas_crisleikeli t ON a.turma_id = t.id
+
+Select d.nome AS 'Nome da Disciplina'
+	  ,p.nome AS 'Nome do Professor'
+FROM disciplinas_crisleikeli d 
+INNER JOIN professores_crisleikeli p ON  p.id = d.professor_id
+
+SELECT d.nome AS 'Nome da Disciplina'
+	 ,p.email AS 'Email'
+FROM disciplinas_crisleikeli d
+INNER JOIN professores_crisleikeli p ON p.id = d.professor_id
+
